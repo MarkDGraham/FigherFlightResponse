e7171c1528ca9cf9ea50c784e93940111742d2f2
John-Busse <78881022+John-Busse@users.noreply.github.com>
2022-04-18 23:03:25 -0700

    Player controller added
    Made player controller, UI, and basic stage

---
+++ Assets/Scripts/PlayerMgr.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMgr : MonoBehaviour
+{
+	// Changing variables
+	public Vector3 position;
+	public Vector3 velocity = Vector3.zero;
+
+	public float speed;
+	public float desiredSpeed;
+	public float heading;
+	public int hull;
+
+	// Constant variables
+	public float acceleration;
+	public float turnRate;
+	public float maxSpeed;
+	public float minSpeed;
+	
+	public GameObject pitchNode;
+
+	// transitional variables
+	float deltaSpeed;
+	float deltaHeading;
+	float deltaPitch;
+	Vector3 currentEuler = Vector3.zero;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// Adjust acceleration
+		deltaSpeed = Input.GetAxis("Acceleration"); // {-1, 0, 1}
+		desiredSpeed += deltaSpeed;
+		desiredSpeed = Utils.Clamp(desiredSpeed, minSpeed, maxSpeed);
+
+		// Adjust yaw
+		deltaHeading = 20 * Input.GetAxis("Yaw");	// {-10, 0, 10}
+		heading += deltaHeading * Time.deltaTime;
+		heading = Utils.DegreeClamp(heading);
+
+		// Adjust pitch
+		currentEuler = pitchNode.transform.localEulerAngles;
+		deltaPitch = 0.5f *Input.GetAxis("Pitch");
+		currentEuler.x += deltaPitch;
+		pitchNode.transform.localEulerAngles = currentEuler;
+
+		// fire weapon
+    }
+}
---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils
+{
+	static float EPSILON = 0.05f;
+	public static bool Approximation(float a, float b)
+	{
+		return Mathf.Abs(a - b) < EPSILON;
+	}
+
+	public static float Clamp(float value, float min, float max)
+	{
+		if (value < min)
+			value = min;
+		else if (value > max)
+			value = max;
+		return value;
+	}
+
+	public static float DegreeClamp(float angle)
+	{
+		if (angle >= 360)
+			angle -= 360;
+		else if (angle < 0)
+			angle += 360;
+
+		return angle;
+	}
+}
